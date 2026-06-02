4bd84f425684d38db4b46d9b30ce8adea2d917b4
srishti <srishtimeow999@gmail.com>
2026-06-02 21:55:08 +0530

    Fix homepage route

--- server/index.js
+++ server/index.js
@@ -21,6 +21,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static(path.join(__dirname, '../public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
